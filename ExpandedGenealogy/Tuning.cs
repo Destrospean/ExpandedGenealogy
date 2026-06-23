6212582edeb1ab22316ece6d87d7ca0bf372e437
CreatorAndrew <andrew.noah.mitchell@gmail.com>
2026-06-23 09:15:46 -0500

    Simplify tuning

--- ExpandedGenealogy/Tuning.cs
+++ ExpandedGenealogy/Tuning.cs
@@ -1,55 +1,14 @@
-﻿using Sims3.SimIFace;
-
-namespace Sims3.Gameplay.Destrospean
+﻿namespace Sims3.Gameplay.Destrospean
 {
     public static class ExpandedGenealogy
     {
-        [Tunable]
-        public static bool kAllowRomanceForHalfRelatives;
+        [Sims3.SimIFace.Tunable]
+        public static bool kAllowRomanceForHalfRelatives, kDenyRomanceWithAncestors, kDenyRomanceWithSiblings, kDenyRomanceWithSiblingsOfAncestors, kDenyRomanceWithStepParents, kDenyRomanceWithStepSiblings, kReplaceRelationsForSimBots, kShow1stCousinsAsCousins, kShowCheatInteractions, kShowHalfRelatives, kShowHalfRelativesAsFullRelatives;
 
-        [Tunable]
-        public static bool kDenyRomanceWithAncestors;
+        [Sims3.SimIFace.Tunable]
+        public static int kMaxDegreeCousinsToShow, kMaxTimesRemovedCousinsToShow, kMinDegreeCousinsToAllowRomance, kMinTimesRemovedCousinsToAllowRomance;
 
-        [Tunable]
-        public static bool kDenyRomanceWithSiblings;
-
-        [Tunable]
-        public static bool kDenyRomanceWithSiblingsOfAncestors;
-
-        [Tunable]
-        public static bool kDenyRomanceWithStepParents;
-
-        [Tunable]
-        public static bool kDenyRomanceWithStepSiblings;
-
-        [Tunable]
-        public static int kMaxDegreeCousinsToShow;
-
-        [Tunable]
-        public static int kMaxTimesRemovedCousinsToShow;
-
-        [Tunable]
-        public static int kMinDegreeCousinsToAllowRomance;
-
-        [Tunable]
+        [Sims3.SimIFace.Tunable]
         public static float kMinRelationshipCoefficientToDenyRomance;
-
-        [Tunable]
-        public static int kMinTimesRemovedCousinsToAllowRomance;
-
-        [Tunable]
-        public static bool kReplaceRelationsForSimBots;
-
-        [Tunable]
-        public static bool kShow1stCousinsAsCousins;
-
-        [Tunable]
-        public static bool kShowCheatInteractions;
-
-        [Tunable]
-        public static bool kShowHalfRelatives;
-
-        [Tunable]
-        public static bool kShowHalfRelativesAsFullRelatives;
     }
 }
